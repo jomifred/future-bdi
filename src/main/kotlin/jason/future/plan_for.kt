10e39f5208e92ef340d5444fab68ee42ca4a4898
Jomi Fred Hubner <jomifred@gmail.com>
2023-06-28 21:37:20 +0200

    Update plan_for.kt

--- src/main/kotlin/jason/future/plan_for.kt
+++ src/main/kotlin/jason/future/plan_for.kt
@@ -1,9 +1,6 @@
 package jason.future
 
-import jason.asSemantics.DefaultInternalAction
-import jason.asSemantics.Event
-import jason.asSemantics.TransitionSystem
-import jason.asSemantics.Unifier
+import jason.asSemantics.*
 import jason.asSyntax.*
 
 class plan_for : DefaultInternalAction() {
@@ -12,32 +9,53 @@ class plan_for : DefaultInternalAction() {
     override fun getMinArgs(): Int = 4
 
     override fun execute(ts: TransitionSystem, un: Unifier, args: Array<out Term>): Any {
-        // prepare arguments
-        val goal     = args[0] as Literal
-        val ag       = ts.ag as ForeseeProblemAgent
-        val strategy = ExplorationStrategy.valueOf( (args[3] as StringTerm).string )
+        println("** start building plan for ${args[0]}")
+        try {
+            // prepare arguments
+            val goal = args[0] as Literal
+            val ag = ts.ag as ForeseeProblemAgent
+            val strategy = ExplorationStrategy.valueOf((args[3] as StringTerm).string)
 
-        val initialPlan = (args[1] as Plan).capply(un)
-        initialPlan.setAsPlanTerm(false)
-        var initialPlanStr =  initialPlan.toString();
-        initialPlanStr = initialPlanStr.substring(0, initialPlanStr.length-1) // remove last "."
-        if (initialPlan.body.planSize == 0)
-            initialPlanStr += " <- "
+            val initialPlan = (args[1] as Plan).capply(un)
+            initialPlan.setAsPlanTerm(false)
+            var initialPlanStr = initialPlan.toString()
+            initialPlanStr = initialPlanStr.substring(0, initialPlanStr.length - 1) // remove last "."
+            if (initialPlan.body.planSize == 0)
+                initialPlanStr += " <- "
+            else
+                initialPlanStr += "; "
 
-        // run search using matrix
-        val search = Search(ag, strategy, ag.envModel())
-        val te = Trigger(Trigger.TEOperator.add, Trigger.TEType.achieve, goal)
-        val relPlans = ts.relevantPlans(te, Event(te, ts.c.selectedIntention))
-        val appPlans = ts.applicablePlans(relPlans)
+            // run search using matrix
+            val search = Search(ag, strategy, ag.envModel())
 
-        search.init(appPlans.get(0), appPlans)
-        search.run()
+            val te = Trigger(Trigger.TEOperator.add, Trigger.TEType.achieve, goal)
+            val relPlans = ts.relevantPlans(te, Event(te, baseIntention(goal)))
+            val appPlans = ts.applicablePlans(relPlans)
 
-        // build the solution
-        val newPlan = ASSyntax.parsePlan("${initialPlanStr} ${ag.planBodyFound}.")
-        newPlan.setAsPlanTerm(true)
+            search.init(appPlans.get(0), appPlans)
+            search.run()
 
-        return un.unifies(args[2],newPlan)
+            // build the new plan
+            val newPlan = ASSyntax.parsePlan("${initialPlanStr} ${ag.planBodyFound}.")
+            newPlan.setAsPlanTerm(true)
+
+            println("** created plan = ${newPlan}")
+
+            return un.unifies(args[2], newPlan)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return false
+        }
     }
 
+    fun baseIntention(goal: Literal) : Intention {
+        val te1 = Trigger(Trigger.TEOperator.add, Trigger.TEType.achieve, Atom("xxx"))
+        val intention = Intention()
+        val evt = Event(te1,intention)
+        intention.push(IntendedMeans(
+            Option(ASSyntax.parsePlan("+!xxx <- !${goal}."), Unifier(), evt),
+            te1)
+        )
+        return intention
+    }
 }
